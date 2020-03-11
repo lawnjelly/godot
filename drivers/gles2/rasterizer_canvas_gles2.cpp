35b639cfb7f0199c3b96925307ffdeec0a44df25
lawnjelly <lawnjelly@gmail.com>
2020-03-11 17:21:38 +0000

    Started on try join function

--- drivers/gles2/rasterizer_canvas_gles2.cpp
+++ drivers/gles2/rasterizer_canvas_gles2.cpp
@@ -1427,11 +1427,6 @@ void RasterizerCanvasGLES2::_canvas_item_render_commands(Item *p_item, Item *cur
 	} // while there are still more batches to fill
 }
 
-bool RasterizerCanvasGLES2::_try_join_item(Item * ci, RIState &ris)
-{
-
-	return false;
-}
 
 
 void RasterizerCanvasGLES2::join_items(Item *p_item_list, int p_z, const Color &p_modulate, Light *p_light, const Transform2D &p_base_transform)
@@ -1505,6 +1500,363 @@ void RasterizerCanvasGLES2::canvas_render_items_implementation(Item *p_item_list
 	}
 
 	state.canvas_shader.set_conditional(CanvasShaderGLES2::USE_SKELETON, false);
+}
+
+
+// this function should duplicate the logic in _canvas_render_item, to decide whether items are similar enough to join
+// i.e. no state changes.
+bool RasterizerCanvasGLES2::_try_join_item(Item * ci, RIState &ris)
+{
+	bool join = true;
+
+	if (ris.current_clip != ci->final_clip_owner) {
+		ris.current_clip = ci->final_clip_owner;
+		join = false;
+	}
+
+	// TODO: copy back buffer
+
+	if (ci->copy_back_buffer) {
+		join = false;
+	}
+
+	RasterizerStorageGLES2::Skeleton *skeleton = NULL;
+
+	{
+		//skeleton handling
+		if (ci->skeleton.is_valid() && storage->skeleton_owner.owns(ci->skeleton)) {
+			skeleton = storage->skeleton_owner.get(ci->skeleton);
+			if (!skeleton->use_2d) {
+				skeleton = NULL;
+			} else {
+				state.skeleton_transform = ris.IG_base_transform * skeleton->base_transform_2d;
+				state.skeleton_transform_inverse = state.skeleton_transform.affine_inverse();
+				state.skeleton_texture_size = Vector2(skeleton->size * 2, 0);
+			}
+		}
+
+		bool use_skeleton = skeleton != NULL;
+		if (ris.prev_use_skeleton != use_skeleton) {
+			ris.rebind_shader = true;
+			state.canvas_shader.set_conditional(CanvasShaderGLES2::USE_SKELETON, use_skeleton);
+			ris.prev_use_skeleton = use_skeleton;
+		}
+
+		if (skeleton) {
+			glActiveTexture(GL_TEXTURE0 + storage->config.max_texture_image_units - 3);
+			glBindTexture(GL_TEXTURE_2D, skeleton->tex_id);
+			state.using_skeleton = true;
+		} else {
+			state.using_skeleton = false;
+		}
+	}
+
+	Item *material_owner = ci->material_owner ? ci->material_owner : ci;
+
+	RID material = material_owner->material;
+	RasterizerStorageGLES2::Material *material_ptr = storage->material_owner.getornull(material);
+
+	if (material != ris.canvas_last_material || ris.rebind_shader) {
+
+		RasterizerStorageGLES2::Shader *shader_ptr = NULL;
+
+		if (material_ptr) {
+			shader_ptr = material_ptr->shader;
+
+			if (shader_ptr && shader_ptr->mode != VS::SHADER_CANVAS_ITEM) {
+				shader_ptr = NULL; // not a canvas item shader, don't use.
+			}
+		}
+
+		if (shader_ptr) {
+			if (shader_ptr->canvas_item.uses_screen_texture) {
+				if (!state.canvas_texscreen_used) {
+					//copy if not copied before
+					_copy_texscreen(Rect2());
+
+					// blend mode will have been enabled so make sure we disable it again later on
+					//last_blend_mode = last_blend_mode != RasterizerStorageGLES2::Shader::CanvasItem::BLEND_MODE_DISABLED ? last_blend_mode : -1;
+				}
+
+				if (storage->frame.current_rt->copy_screen_effect.color) {
+					glActiveTexture(GL_TEXTURE0 + storage->config.max_texture_image_units - 4);
+					glBindTexture(GL_TEXTURE_2D, storage->frame.current_rt->copy_screen_effect.color);
+				}
+			}
+
+			if (shader_ptr != ris.shader_cache) {
+
+				if (shader_ptr->canvas_item.uses_time) {
+					VisualServerRaster::redraw_request();
+				}
+
+				state.canvas_shader.set_custom_shader(shader_ptr->custom_code_id);
+				state.canvas_shader.bind();
+			}
+
+			int tc = material_ptr->textures.size();
+			Pair<StringName, RID> *textures = material_ptr->textures.ptrw();
+
+			ShaderLanguage::ShaderNode::Uniform::Hint *texture_hints = shader_ptr->texture_hints.ptrw();
+
+			for (int i = 0; i < tc; i++) {
+
+				glActiveTexture(GL_TEXTURE0 + i);
+
+				RasterizerStorageGLES2::Texture *t = storage->texture_owner.getornull(textures[i].second);
+
+				if (!t) {
+
+					switch (texture_hints[i]) {
+						case ShaderLanguage::ShaderNode::Uniform::HINT_BLACK_ALBEDO:
+						case ShaderLanguage::ShaderNode::Uniform::HINT_BLACK: {
+							glBindTexture(GL_TEXTURE_2D, storage->resources.black_tex);
+						} break;
+						case ShaderLanguage::ShaderNode::Uniform::HINT_ANISO: {
+							glBindTexture(GL_TEXTURE_2D, storage->resources.aniso_tex);
+						} break;
+						case ShaderLanguage::ShaderNode::Uniform::HINT_NORMAL: {
+							glBindTexture(GL_TEXTURE_2D, storage->resources.normal_tex);
+						} break;
+						default: {
+							glBindTexture(GL_TEXTURE_2D, storage->resources.white_tex);
+						} break;
+					}
+
+					continue;
+				}
+
+				if (t->redraw_if_visible) {
+					VisualServerRaster::redraw_request();
+				}
+
+				t = t->get_ptr();
+
+#ifdef TOOLS_ENABLED
+				if (t->detect_normal && texture_hints[i] == ShaderLanguage::ShaderNode::Uniform::HINT_NORMAL) {
+					t->detect_normal(t->detect_normal_ud);
+				}
+#endif
+				if (t->render_target)
+					t->render_target->used_in_frame = true;
+
+				glBindTexture(t->target, t->tex_id);
+			}
+
+		} else {
+			state.canvas_shader.set_custom_shader(0);
+			state.canvas_shader.bind();
+		}
+		state.canvas_shader.use_material((void *)material_ptr);
+
+		ris.shader_cache = shader_ptr;
+
+		ris.canvas_last_material = material;
+
+		ris.rebind_shader = false;
+	}
+
+	int blend_mode = ris.shader_cache ? ris.shader_cache->canvas_item.blend_mode : RasterizerStorageGLES2::Shader::CanvasItem::BLEND_MODE_MIX;
+	bool unshaded = ris.shader_cache && (ris.shader_cache->canvas_item.light_mode == RasterizerStorageGLES2::Shader::CanvasItem::LIGHT_MODE_UNSHADED || (blend_mode != RasterizerStorageGLES2::Shader::CanvasItem::BLEND_MODE_MIX && blend_mode != RasterizerStorageGLES2::Shader::CanvasItem::BLEND_MODE_PMALPHA));
+	bool reclip = false;
+
+	if (ris.last_blend_mode != blend_mode) {
+
+		switch (blend_mode) {
+
+			case RasterizerStorageGLES2::Shader::CanvasItem::BLEND_MODE_MIX: {
+				glBlendEquation(GL_FUNC_ADD);
+				if (storage->frame.current_rt && storage->frame.current_rt->flags[RasterizerStorage::RENDER_TARGET_TRANSPARENT]) {
+					glBlendFuncSeparate(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
+				} else {
+					glBlendFuncSeparate(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_ZERO, GL_ONE);
+				}
+
+			} break;
+			case RasterizerStorageGLES2::Shader::CanvasItem::BLEND_MODE_ADD: {
+
+				glBlendEquation(GL_FUNC_ADD);
+				if (storage->frame.current_rt && storage->frame.current_rt->flags[RasterizerStorage::RENDER_TARGET_TRANSPARENT]) {
+					glBlendFuncSeparate(GL_SRC_ALPHA, GL_ONE, GL_SRC_ALPHA, GL_ONE);
+				} else {
+					glBlendFuncSeparate(GL_SRC_ALPHA, GL_ONE, GL_ZERO, GL_ONE);
+				}
+
+			} break;
+			case RasterizerStorageGLES2::Shader::CanvasItem::BLEND_MODE_SUB: {
+
+				glBlendEquation(GL_FUNC_REVERSE_SUBTRACT);
+				if (storage->frame.current_rt && storage->frame.current_rt->flags[RasterizerStorage::RENDER_TARGET_TRANSPARENT]) {
+					glBlendFuncSeparate(GL_SRC_ALPHA, GL_ONE, GL_SRC_ALPHA, GL_ONE);
+				} else {
+					glBlendFuncSeparate(GL_SRC_ALPHA, GL_ONE, GL_ZERO, GL_ONE);
+				}
+			} break;
+			case RasterizerStorageGLES2::Shader::CanvasItem::BLEND_MODE_MUL: {
+				glBlendEquation(GL_FUNC_ADD);
+				if (storage->frame.current_rt && storage->frame.current_rt->flags[RasterizerStorage::RENDER_TARGET_TRANSPARENT]) {
+					glBlendFuncSeparate(GL_DST_COLOR, GL_ZERO, GL_DST_ALPHA, GL_ZERO);
+				} else {
+					glBlendFuncSeparate(GL_DST_COLOR, GL_ZERO, GL_ZERO, GL_ONE);
+				}
+			} break;
+			case RasterizerStorageGLES2::Shader::CanvasItem::BLEND_MODE_PMALPHA: {
+				glBlendEquation(GL_FUNC_ADD);
+				if (storage->frame.current_rt && storage->frame.current_rt->flags[RasterizerStorage::RENDER_TARGET_TRANSPARENT]) {
+					glBlendFuncSeparate(GL_ONE, GL_ONE_MINUS_SRC_ALPHA, GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
+				} else {
+					glBlendFuncSeparate(GL_ONE, GL_ONE_MINUS_SRC_ALPHA, GL_ZERO, GL_ONE);
+				}
+			} break;
+		}
+	}
+
+	state.uniforms.final_modulate = unshaded ? ci->final_modulate : Color(ci->final_modulate.r * ris.IG_modulate.r, ci->final_modulate.g * ris.IG_modulate.g, ci->final_modulate.b * ris.IG_modulate.b, ci->final_modulate.a * ris.IG_modulate.a);
+
+	state.uniforms.modelview_matrix = ci->final_transform;
+	state.uniforms.extra_matrix = Transform2D();
+
+	_set_uniforms();
+
+	if (unshaded || (state.uniforms.final_modulate.a > 0.001 && (!ris.shader_cache || ris.shader_cache->canvas_item.light_mode != RasterizerStorageGLES2::Shader::CanvasItem::LIGHT_MODE_LIGHT_ONLY) && !ci->light_masked))
+		_canvas_item_render_commands(ci, NULL, reclip, material_ptr);
+
+	ris.rebind_shader = true; // hacked in for now.
+
+	if ((blend_mode == RasterizerStorageGLES2::Shader::CanvasItem::BLEND_MODE_MIX || blend_mode == RasterizerStorageGLES2::Shader::CanvasItem::BLEND_MODE_PMALPHA) && ris.IG_light && !unshaded) {
+
+		Light *light = ris.IG_light;
+		bool light_used = false;
+		VS::CanvasLightMode mode = VS::CANVAS_LIGHT_MODE_ADD;
+		state.uniforms.final_modulate = ci->final_modulate; // remove the canvas modulate
+
+		while (light) {
+
+			if (ci->light_mask & light->item_mask && ris.IG_z >= light->z_min && ris.IG_z <= light->z_max && ci->global_rect_cache.intersects_transformed(light->xform_cache, light->rect_cache)) {
+
+				//intersects this light
+
+				if (!light_used || mode != light->mode) {
+
+					mode = light->mode;
+
+					switch (mode) {
+
+						case VS::CANVAS_LIGHT_MODE_ADD: {
+							glBlendEquation(GL_FUNC_ADD);
+							glBlendFunc(GL_SRC_ALPHA, GL_ONE);
+
+						} break;
+						case VS::CANVAS_LIGHT_MODE_SUB: {
+							glBlendEquation(GL_FUNC_REVERSE_SUBTRACT);
+							glBlendFunc(GL_SRC_ALPHA, GL_ONE);
+						} break;
+						case VS::CANVAS_LIGHT_MODE_MIX:
+						case VS::CANVAS_LIGHT_MODE_MASK: {
+							glBlendEquation(GL_FUNC_ADD);
+							glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+						} break;
+					}
+				}
+
+				if (!light_used) {
+
+					state.canvas_shader.set_conditional(CanvasShaderGLES2::USE_LIGHTING, true);
+					light_used = true;
+				}
+
+				bool has_shadow = light->shadow_buffer.is_valid() && ci->light_mask & light->item_shadow_mask;
+
+				state.canvas_shader.set_conditional(CanvasShaderGLES2::USE_SHADOWS, has_shadow);
+				if (has_shadow) {
+					state.canvas_shader.set_conditional(CanvasShaderGLES2::SHADOW_USE_GRADIENT, light->shadow_gradient_length > 0);
+					state.canvas_shader.set_conditional(CanvasShaderGLES2::SHADOW_FILTER_NEAREST, light->shadow_filter == VS::CANVAS_LIGHT_FILTER_NONE);
+					state.canvas_shader.set_conditional(CanvasShaderGLES2::SHADOW_FILTER_PCF3, light->shadow_filter == VS::CANVAS_LIGHT_FILTER_PCF3);
+					state.canvas_shader.set_conditional(CanvasShaderGLES2::SHADOW_FILTER_PCF5, light->shadow_filter == VS::CANVAS_LIGHT_FILTER_PCF5);
+					state.canvas_shader.set_conditional(CanvasShaderGLES2::SHADOW_FILTER_PCF7, light->shadow_filter == VS::CANVAS_LIGHT_FILTER_PCF7);
+					state.canvas_shader.set_conditional(CanvasShaderGLES2::SHADOW_FILTER_PCF9, light->shadow_filter == VS::CANVAS_LIGHT_FILTER_PCF9);
+					state.canvas_shader.set_conditional(CanvasShaderGLES2::SHADOW_FILTER_PCF13, light->shadow_filter == VS::CANVAS_LIGHT_FILTER_PCF13);
+				}
+
+				state.canvas_shader.bind();
+				state.using_light = light;
+				state.using_shadow = has_shadow;
+
+				//always re-set uniforms, since light parameters changed
+				_set_uniforms();
+				state.canvas_shader.use_material((void *)material_ptr);
+
+				glActiveTexture(GL_TEXTURE0 + storage->config.max_texture_image_units - 4);
+				RasterizerStorageGLES2::Texture *t = storage->texture_owner.getornull(light->texture);
+				if (!t) {
+					glBindTexture(GL_TEXTURE_2D, storage->resources.white_tex);
+				} else {
+					t = t->get_ptr();
+
+					glBindTexture(t->target, t->tex_id);
+				}
+
+				glActiveTexture(GL_TEXTURE0);
+				_canvas_item_render_commands(ci, NULL, reclip, material_ptr); //redraw using light
+
+				state.using_light = NULL;
+			}
+
+			light = light->next_ptr;
+		}
+
+		if (light_used) {
+
+			state.canvas_shader.set_conditional(CanvasShaderGLES2::USE_LIGHTING, false);
+			state.canvas_shader.set_conditional(CanvasShaderGLES2::USE_SHADOWS, false);
+			state.canvas_shader.set_conditional(CanvasShaderGLES2::SHADOW_FILTER_NEAREST, false);
+			state.canvas_shader.set_conditional(CanvasShaderGLES2::SHADOW_FILTER_PCF3, false);
+			state.canvas_shader.set_conditional(CanvasShaderGLES2::SHADOW_FILTER_PCF5, false);
+			state.canvas_shader.set_conditional(CanvasShaderGLES2::SHADOW_FILTER_PCF7, false);
+			state.canvas_shader.set_conditional(CanvasShaderGLES2::SHADOW_FILTER_PCF9, false);
+			state.canvas_shader.set_conditional(CanvasShaderGLES2::SHADOW_FILTER_PCF13, false);
+
+			state.canvas_shader.bind();
+
+			ris.last_blend_mode = -1;
+
+			/*
+			//this is set again, so it should not be needed anyway?
+			state.canvas_item_modulate = unshaded ? ci->final_modulate : Color(
+						ci->final_modulate.r * p_modulate.r,
+						ci->final_modulate.g * p_modulate.g,
+						ci->final_modulate.b * p_modulate.b,
+						ci->final_modulate.a * p_modulate.a );
+
+
+			state.canvas_shader.set_uniform(CanvasShaderGLES2::MODELVIEW_MATRIX,state.final_transform);
+			state.canvas_shader.set_uniform(CanvasShaderGLES2::EXTRA_MATRIX,Transform2D());
+			state.canvas_shader.set_uniform(CanvasShaderGLES2::FINAL_MODULATE,state.canvas_item_modulate);
+
+			glBlendEquation(GL_FUNC_ADD);
+
+			if (storage->frame.current_rt->flags[RasterizerStorage::RENDER_TARGET_TRANSPARENT]) {
+				glBlendFuncSeparate(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
+			} else {
+				glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+			}
+
+			//@TODO RESET canvas_blend_mode
+			*/
+		}
+	}
+
+	if (reclip) {
+		glEnable(GL_SCISSOR_TEST);
+		int y = storage->frame.current_rt->height - (ris.current_clip->final_clip_rect.position.y + ris.current_clip->final_clip_rect.size.y);
+		if (storage->frame.current_rt->flags[RasterizerStorage::RENDER_TARGET_VFLIP])
+			y = ris.current_clip->final_clip_rect.position.y;
+		glScissor(ris.current_clip->final_clip_rect.position.x, y, ris.current_clip->final_clip_rect.size.width, ris.current_clip->final_clip_rect.size.height);
+	}
+
+
+	return join;
 }
 
 
