31451334f5d79254fef0f54262351ddd36eb378e
lawnjelly <lawnjelly@gmail.com>
2020-06-04 12:22:49 +0100

    LPortal - current state

---
+++ modules/lportal/larea.h
@@ -0,0 +1,38 @@
+#pragma once
+
+//	Copyright (c) 2019 Lawnjelly
+
+//	Permission is hereby granted, free of charge, to any person obtaining a copy
+//	of this software and associated documentation files (the "Software"), to deal
+//	in the Software without restriction, including without limitation the rights
+//	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//	copies of the Software, and to permit persons to whom the Software is
+//	furnished to do so, subject to the following conditions:
+
+//	The above copyright notice and this permission notice shall be included in all
+//	copies or substantial portions of the Software.
+
+//	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//	SOFTWARE.
+
+#include "core/ustring.h"
+
+class LArea
+{
+public:
+	void Create(String szName);
+
+	String m_szName;
+
+	int m_iFirstRoom;
+	int m_iNumRooms;
+
+	// each area contains a list of global lights that affect this area
+	int m_iFirstLight;
+	int m_iNumLights;
+};
---
+++ modules/lportal/lplanes_pool.cpp
@@ -0,0 +1,41 @@
+#include "lplanes_pool.h"
+
+LPlanesPool::LPlanesPool()
+{
+	Reset();
+
+	// preallocate the vectors to a reasonable size
+	for (int n=0; n<POOL_MAX; n++)
+	{
+		m_Planes[n].reserve(32);
+	}
+}
+
+void LPlanesPool::Reset()
+{
+	for (int n=0; n<POOL_MAX; n++)
+	{
+		m_ucFreeList[n] = POOL_MAX - n - 1;
+	}
+
+	m_uiNumFree = POOL_MAX;
+}
+
+unsigned int LPlanesPool::Request()
+{
+	if (!m_uiNumFree)
+		return -1;
+
+	m_uiNumFree--;
+	return m_ucFreeList[m_uiNumFree];
+
+}
+
+void LPlanesPool::Free(unsigned int ui)
+{
+	assert (ui <= POOL_MAX);
+	assert (m_uiNumFree < POOL_MAX);
+
+	m_ucFreeList[m_uiNumFree] = ui;
+	m_uiNumFree++;
+}
