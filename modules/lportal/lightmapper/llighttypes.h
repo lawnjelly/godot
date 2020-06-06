f9c5e694f02bb797a705b7cf6bf29e02a5110c7a
lawnjelly <lawnjelly@gmail.com>
2020-06-06 18:43:34 +0100

    Basics of lightmapper. But not tested.

--- modules/lportal/lightmapper/llighttypes.h
+++ modules/lportal/lightmapper/llighttypes.h
@@ -6,6 +6,8 @@ class Tri
 {
 public:
 	Vector3 pos[3];
+
+	void FindBarycentric(const Vector3 &pt, float &u, float &v, float &w) const;
 };
 
 
@@ -16,6 +18,7 @@ public:
 	Vector3 d; // direction
 
 	bool TestIntersect(const Tri &tri, float &t) const;
+	void FindIntersect(const Tri &tri, float t, float &u, float &v, float &w) const;
 
 };
 
@@ -23,9 +26,42 @@ class UVTri
 {
 public:
 	Vector2 uv[3];
+
+	void FindUVBarycentric(Vector2 &res, float u, float v, float w) const;
 };
 
 
+inline void UVTri::FindUVBarycentric(Vector2 &res, float u, float v, float w) const
+{
+	res = (uv[0] * u) + (uv[1] * v) + (uv[2] * w);
+}
+
+
+inline void Tri::FindBarycentric(const Vector3 &pt, float &u, float &v, float &w) const
+{
+	const Vector3 &a = pos[0];
+	const Vector3 &b = pos[1];
+	const Vector3 &c = pos[2];
+
+	// we can alternatively precache
+	Vector3 v0 = b - a, v1 = c - a, v2 = pt - a;
+    float d00 = v0.dot(v0);
+    float d01 = v0.dot(v1);
+    float d11 = v1.dot(v1);
+    float d20 = v2.dot(v0);
+    float d21 = v2.dot(v1);
+    float invDenom = 1.0f / (d00 * d11 - d01 * d01);
+    v = (d11 * d20 - d01 * d21) * invDenom;
+    w = (d00 * d21 - d01 * d20) * invDenom;
+    u = 1.0f - v - w;
+}
+
+// returns barycentric, assumes we have already tested and got a collision
+inline void Ray::FindIntersect(const Tri &tri, float t, float &u, float &v, float &w) const
+{
+	Vector3 pt = o + (d * t);
+	tri.FindBarycentric(pt, u, v, w);
+}
 
 
 inline bool Ray::TestIntersect(const Tri &tri, float &t) const
