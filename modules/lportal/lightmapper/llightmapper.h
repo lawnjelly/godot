b1b78409d160dec5eb87757954553a68132618d8
lawnjelly <lawnjelly@gmail.com>
2020-06-13 07:09:44 +0100

    Backward tracing experiments

--- modules/lportal/lightmapper/llightmapper.h
+++ modules/lportal/lightmapper/llightmapper.h
@@ -31,23 +31,34 @@ class LLightMapper : public Spatial
 
 	GDCLASS(LLightMapper, Spatial);
 public:
-	bool lightmap_mesh(Node * pMeshInstance, Object * pOutputImage, int num_rays, float power);
+	bool lightmap_mesh(Node * pMeshInstance, Object * pOutputImage, int num_rays, float power, float bounce_power);
 
 private:
 	bool LightmapMesh(const MeshInstance &mi, Image &output_image);
 
 private:
 	void FindLights(const MeshInstance &mi);
-	void ProcessTexels();
 	void ProcessLight(int light_id);
-	void ProcessTexel(int x, int y);
 	void PrepareImageMaps();
-	void ProcessRay(const LM::Ray &r, int depth, float power, int dest_tri_id = 0, const Vector2i * pUV = 0);
+	void ProcessRay(LM::Ray r, int depth, float power, int dest_tri_id = 0, const Vector2i * pUV = 0);
+
+
+	void ProcessTexels();
+	void ProcessTexel(int tx, int ty);
+	void ProcessSubTexel(float fx, float fy);
+	float ProcessTexel_Light(int light_id, const Vector3 &ptDest, uint32_t tri_ignore);
+
+	void ProcessTexels_Bounce();
+	float ProcessTexel_Bounce(int x, int y);
 
 	void WriteOutputImage(Image &output_image);
 
 	// luminosity
 	LM::LightImage<float> m_Image_L;
+
+	// for bounces
+	LM::LightImage<float> m_Image_L_mirror;
+
 	LM::LightImage<uint32_t> m_Image_ID_p1;
 	LM::LightImage<Vector3> m_Image_Barycentric;
 
@@ -64,6 +75,7 @@ private:
 	// params
 	int m_Settings_NumRays;
 	float m_Settings_RayPower;
+	float m_Settings_BouncePower;
 
 protected:
 	static void _bind_methods();
