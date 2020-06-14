129243a1d08d1b1cee2393f50441b306876230a1
lawnjelly <lawnjelly@gmail.com>
2020-06-14 19:10:05 +0100

    Added editor plugin for baking

--- modules/lportal/lightmapper/llightmapper.h
+++ modules/lportal/lightmapper/llightmapper.h
@@ -26,18 +26,47 @@ class LLightMapper : public Spatial
 		float indirect_energy;
 		float range;
 		float spot_angle;
-		Light * m_pLight;
+		const Light * m_pLight;
 	};
 
 	GDCLASS(LLightMapper, Spatial);
 public:
-	bool lightmap_mesh(Node * pMeshInstance, Object * pOutputImage, int num_rays, float power, float bounce_power);
+	enum eMode
+	{
+		MODE_FORWARD,
+		MODE_BACKWARD,
+	};
+
+	void lightmap_set_params(int num_rays, float power, float bounce_power);
+	bool lightmap_mesh(Node * pMeshInstance, Node * pLightRoot, Object * pOutputImage);
+
+	void set_mode(LLightMapper::eMode p_mode);
+	LLightMapper::eMode get_mode() const;
+
+	void set_mesh_path(const NodePath &p_path);
+	NodePath get_mesh_path() const;
+	void set_lights_path(const NodePath &p_path);
+	NodePath get_lights_path() const;
+
+	void set_num_rays(int num_rays);
+	int get_num_rays() const;
+
+	void set_num_bounces(int num_bounces);
+	int get_num_bounces() const;
+
+	void set_ray_power(float ray_power);
+	float get_ray_power() const;
+
+	void set_bounce_power(float bounce_power);
+	float get_bounce_power() const;
 
 private:
-	bool LightmapMesh(const MeshInstance &mi, Image &output_image);
+	bool LightmapMesh(const MeshInstance &mi, const Spatial &light_root, Image &output_image);
 
 private:
-	void FindLights(const MeshInstance &mi);
+	void FindLights_Recursive(const Node * pNode);
+	void FindLight(const Node * pNode);
+
 	void ProcessLight(int light_id);
 	void PrepareImageMaps();
 	void ProcessRay(LM::Ray r, int depth, float power, int dest_tri_id = 0, const Vector2i * pUV = 0);
@@ -74,10 +103,17 @@ private:
 
 	// params
 	int m_Settings_NumRays;
+	int m_Settings_NumBounces;
 	float m_Settings_RayPower;
 	float m_Settings_BouncePower;
+	eMode m_Settings_Mode;
+
+	NodePath m_Settings_Path_Mesh;
+	NodePath m_Settings_Path_Lights;
 
 protected:
+	LLightMapper();
 	static void _bind_methods();
 };
 
+VARIANT_ENUM_CAST(LLightMapper::eMode);
