287b431218ed7131dc91033392983f1a71384089
lawnjelly <lawnjelly@gmail.com>
2020-06-19 08:46:01 +0100

    Start of SIMD function

---
+++ modules/lportal/lightmapper/llighttests_simd.cpp
@@ -0,0 +1,9 @@
+#include "llighttests_simd.h"
+
+using namespace LM;
+
+void LightTests_SIMD::TestIntersect4(const Tri *tris[4], const Ray &ray, float &r_nearest_t, int &r_nearest_tri) const
+{
+
+
+}
