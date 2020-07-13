71eb860bc448eba7e97cfdb58f675f2a92554656
lawnjelly <lawnjelly@gmail.com>
2020-07-13 15:07:11 +0100

    Unmerger added.

--- modules/llightmap/lmerger.h
+++ modules/llightmap/lmerger.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "scene/3d/mesh_instance.h"
+#include "core/vector.h"
+
 
 namespace LM {
 
@@ -19,7 +21,7 @@ private:
 	static bool xatlas_unwrap(float p_texel_size, const float *p_vertices, const float *p_normals, int p_vertex_count, const int *p_indices, const int *p_face_materials, int p_index_count, float **r_uv, int **r_vertex, int *r_vertex_count, int **r_index, int *r_index_count, int *r_size_hint_x, int *r_size_hint_y);
 
 
-
+public:
 	Vector<MeshInstance *> m_Meshes;
 
 };
