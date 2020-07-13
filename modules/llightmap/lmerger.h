dea02895a565672096571cfc49e3239281d4be1f
lawnjelly <lawnjelly@gmail.com>
2020-07-13 11:31:45 +0100

    Bit more on merging.

--- modules/llightmap/lmerger.h
+++ modules/llightmap/lmerger.h
@@ -12,6 +12,13 @@ public:
 
 private:
 	void FindMeshes(Spatial * pNode);
+	void MergeMeshes(MeshInstance &merged);
+	void Merge_MeshInstance(const MeshInstance &mi, PoolVector<Vector3> &verts, PoolVector<Vector3> &norms, PoolVector<int> &inds);
+	bool LightmapUnwrap(Ref<ArrayMesh> am, const Transform &trans);
+
+	static bool xatlas_unwrap(float p_texel_size, const float *p_vertices, const float *p_normals, int p_vertex_count, const int *p_indices, const int *p_face_materials, int p_index_count, float **r_uv, int **r_vertex, int *r_vertex_count, int **r_index, int *r_index_count, int *r_size_hint_x, int *r_size_hint_y);
+
+
 
 	Vector<MeshInstance *> m_Meshes;
 
