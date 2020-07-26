3ded9d3c9c2b724f30fef6b63256f5b63fc6619a
lawnjelly <lawnjelly@gmail.com>
2020-07-26 11:41:44 +0100

    Starting to create LMaterials.

---
+++ modules/llightmap/lmaterials.cpp
@@ -0,0 +1,19 @@
+#include "lmaterials.h"
+
+namespace LM
+{
+
+int LMaterials::FindOrCreateMaterial(Ref<Mesh> rmesh, int surf_id)
+{
+
+	return 0;
+}
+
+bool LMaterials::FindColors(int mat_id, const Vector2 &uv, Color &aldedo)
+{
+
+	return true;
+}
+
+
+} // namespace
