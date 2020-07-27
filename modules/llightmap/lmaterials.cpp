2d5e35599655910cb7be8d904f0fc90f6f30b5d6
lawnjelly <lawnjelly@gmail.com>
2020-07-27 08:14:52 +0100

    Finding custom shader albedo - not totally working.

--- modules/llightmap/lmaterials.cpp
+++ modules/llightmap/lmaterials.cpp
@@ -71,33 +71,93 @@ int LMaterials::FindOrCreateMaterial(const MeshInstance &mi, Ref<Mesh> rmesh, in
 	pNew->Create();
 	pNew->pGodotMaterial = pSrcMaterial;
 
-	//	int mesh_id = m_Tri_MeshIDs[tri_id];
-	//	int surf_id = m_Tri_SurfIDs[tri_id];
+	// spatial material?
+	Ref<SpatialMaterial> spatial_mat = src_material;
+	Ref<Texture> albedo_tex;
 
-	//	const MeshInstance &mi = *m_Meshes[mesh_id];
-	//	Ref<Mesh> rmesh = mi.get_mesh();
-
-	Ref<SpatialMaterial> mat = src_material;
-
-	if (mat.is_valid())
+	if (spatial_mat.is_valid())
 	{
-		Ref<Texture> albedo_tex = mat->get_texture(SpatialMaterial::TEXTURE_ALBEDO);
-		Ref<Image> img_albedo;
-		if (albedo_tex.is_valid())
-		{
-			img_albedo = albedo_tex->get_data();
-			pNew->pAlbedo = _get_bake_texture(img_albedo, mat->get_albedo(), Color(0, 0, 0)); // albedo texture, color is multiplicative
-			//albedo_texture = _get_bake_texture(img_albedo, size, mat->get_albedo(), Color(0, 0, 0)); // albedo texture, color is multiplicative
-		} else
-		{
-			//albedo_texture = _get_bake_texture(img_albedo, size, Color(1, 1, 1), mat->get_albedo()); // no albedo texture, color is additive
-		}
+		albedo_tex = spatial_mat->get_texture(SpatialMaterial::TEXTURE_ALBEDO);
+	}
+	else
+	{
+		// shader material?
+		Variant shader_tex = FindShaderTex(src_material);
+		Ref<Texture> albedo_tex = shader_tex;
+	} // not spatial mat
+
+	Ref<Image> img_albedo;
+	if (albedo_tex.is_valid())
+	{
+		img_albedo = albedo_tex->get_data();
+		pNew->pAlbedo = _get_bake_texture(img_albedo, spatial_mat->get_albedo(), Color(0, 0, 0)); // albedo texture, color is multiplicative
+		//albedo_texture = _get_bake_texture(img_albedo, size, mat->get_albedo(), Color(0, 0, 0)); // albedo texture, color is multiplicative
+	} else
+	{
+		//albedo_texture = _get_bake_texture(img_albedo, size, Color(1, 1, 1), mat->get_albedo()); // no albedo texture, color is additive
 	}
 
 
 	// returns the new material ID plus 1
 	return m_Materials.size();
 }
+
+Variant LMaterials::FindShaderTex(Ref<Material> src_material)
+{
+
+	Ref<ShaderMaterial> shader_mat = src_material;
+
+	if (!shader_mat.is_valid())
+		return Variant::NIL;
+
+	// get the shader
+	Ref<Shader> shader = shader_mat->get_shader();
+	if (!shader.is_valid())
+		return Variant::NIL;
+
+	// find the most likely albedo texture
+	List<PropertyInfo> plist;
+	shader->get_param_list(&plist);
+
+	String sz_first_obj_param;
+
+	for (List<PropertyInfo>::Element *E = plist.front(); E; E = E->next()) {
+		String szName = E->get().name;
+		Variant::Type t = E->get().type;
+//				print_line("shader param : " + szName);
+//				print_line("shader type : " + String(Variant(t)));
+		if (t == Variant::OBJECT)
+		{
+			sz_first_obj_param = szName;
+			break;
+		}
+
+		//r_options->push_back(quote_style + E->get().name.replace_first("shader_param/", "") + quote_style);
+	}
+
+	if (sz_first_obj_param == "")
+		return Variant::NIL;
+
+	StringName pr = shader->remap_param(sz_first_obj_param);
+	if (!pr) {
+		String n = sz_first_obj_param;
+		if (n.find("param/") == 0) { //backwards compatibility
+			pr = n.substr(6, n.length());
+		}
+		if (n.find("shader_param/") == 0) { //backwards compatibility
+			pr = n.replace_first("shader_param/", "");
+		}
+	}
+
+	if (!pr)
+		return Variant::NIL;
+
+	Variant param = shader_mat->get_shader_param(pr);
+
+	print_line("\tparam is " + String(param));
+	return param;
+}
+
 
 LTexture * LMaterials::_get_bake_texture(Ref<Image> p_image, const Color &p_color_mul, const Color &p_color_add)
 {
