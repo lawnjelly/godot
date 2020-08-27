b3f921583f3cde15e19d4ea18fd2278d40c0687f
lawnjelly <lawnjelly@gmail.com>
2020-08-27 09:21:33 +0100

    Removed GCC only flags from SIMD SCub.

--- modules/llightmap/llightmapper.cpp
+++ modules/llightmap/llightmapper.cpp
@@ -659,6 +659,27 @@ void LightMapper::ProcessTexel_Light(int light_id, const Vector3 &ptSource, cons
 
 void LightMapper::ProcessLightProbes()
 {
+	// get the aabb (smallest) of the world bound.
+	AABB bb = GetTracer().GetWorldBound_contracted();
+
+	// can't make probes for a non existant world
+	if (bb.get_shortest_axis_size() <= 0.0f)
+		return;
+
+	// start hard coded
+	Vec3i dims;
+	dims.Set(4, 4, 1);
+
+	Vector3 voxel_dims;
+	dims.To(voxel_dims);
+
+	// voxel dimensions and start point
+	Vector3 voxel_size;
+	voxel_size = bb.size / voxel_dims;
+
+	// start point is offset by half a voxel
+	Vector3 ptMin = bb.position + (voxel_size * 0.5f);
+
 
 }
 
