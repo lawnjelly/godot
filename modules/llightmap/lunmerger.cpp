48422040915255ee5564dd750893c043fbb200ce
lawnjelly <lawnjelly@gmail.com>
2020-09-09 17:54:09 +0100

    Some tidying up of the new unmerging.

--- modules/llightmap/lunmerger.cpp
+++ modules/llightmap/lunmerger.cpp
@@ -147,120 +147,45 @@ bool UnMerger::UnMerge_Mesh(MeshInstance &mi, LMerged &merged)
 #endif
 
 		// find matching face
-		//		int miCount = 0;
-	//	bool bMatchFound = false;
 		float best_goodness_of_fit = FLT_MAX;
 		int best_matching_merged_face = -1;
 
+		for (int mf=0; mf<nMergedFaces; mf++)
+		{
+			// construct merged lface
+			// no need to standardize, this is done as a pre-process now.
+			LFace mlf = merged.m_LFaces[mf];
 
-//		for (int repeat=0; repeat<1; repeat++)
-//		{
+			// determine how good a match these 2 faces are
+			float goodness_of_fit = DoFacesMatch(lf, mlf);
 
-			for (int mf=0; mf<nMergedFaces; mf++)
+			// is it a better fit than the best match so far?
+			if (goodness_of_fit < best_goodness_of_fit)
 			{
-				// construct merged lface
-				LFace mlf = merged.m_LFaces[mf];
-				mlf.StandardizeOrder();
+				best_goodness_of_fit = goodness_of_fit;
+				best_matching_merged_face = mf;
 
-				//			for (int c=0; c<3; c++)
-				//			{
-				//				int ind = merged.m_Inds[miCount++];
-				//				mlf.m_Pos[c] = merged.m_Verts[ind];
-				//				mlf.m_Norm[c] = merged.m_Norms[ind];
-
-				//				mlf.m_index[c] = ind;
-				//			}
-
-
-				//int match = DoFacesMatch(lf, mlf);
-
-				float goodness_of_fit = DoFacesMatch_New(lf, mlf);
-
-				if (goodness_of_fit < best_goodness_of_fit)
-				{
-					best_goodness_of_fit = goodness_of_fit;
-					best_matching_merged_face = mf;
-
-					// special case .. perfect goodness of fit
-					if (goodness_of_fit == 0.0f)
-						break;
-				}
-
-				/*
-				if (match != -1)
-				{
-					// we found a match in the merged mesh! transfer the UV2s
-					bMatchFound = true;
-
-					//sz += " match found offset " + itos(match);
-
-					// find the corresponding uv2s in the merged mesh face and add them (taking into account offset)
-					Vector2 found_uvs[3];
-					for (int c=0; c<3; c++)
-					{
-						found_uvs[c] = merged.m_UV2s[mlf.m_index[c]];
-					}
-
-					// add them
-					for (int c=0; c<3; c++)
-					{
-						int which = (c + match)	% 3;
-
-						// index for the uv2 should be the same as the vertex index in the 'to mesh' face
-						int ind = lf.m_index[c];
-						uv2s.set(ind, found_uvs[which]);
-
-						{
-							// construct the unique vert
-							LVert uvert;
-							int orig_ind = lf.m_index[c];
-							uvert.m_Pos = verts[orig_ind];
-							uvert.m_Norm = norms[orig_ind];
-							uvert.m_UV = uv1s[orig_ind];
-							uvert.m_UV2 = found_uvs[which];
-
-							// find it or add to the list
-							int ind_uni_vert = FindOrAddVert(UniqueVerts, uvert);
-
-							// add the index to form the triangle list
-							UniqueIndices.push_back(ind_uni_vert);
-						}
-					}
-
+				// special case .. perfect goodness of fit
+				if (goodness_of_fit == 0.0f)
 					break;
-				}
-				*/
-			} // for through merged faces
-
-			// special case
-//			if (!bMatchFound)
-			if (best_matching_merged_face == -1)
-			{
-				// add some dummy uv2s
-				//			uv2s.push_back(Vector2(0, 0));
-				//			uv2s.push_back(Vector2(0, 0));
-				//			uv2s.push_back(Vector2(0, 0));
-
-				String sz = "\tface " + itos(f);
-				sz += " no match";
-				print_line (sz);
 			}
-			else
-			{
-				LFace mlf = merged.m_LFaces[best_matching_merged_face];
-				mlf.StandardizeOrder();
 
-				// double check this is correct
-				int match;
-				if (mlf.offset >= lf.offset)
-					match = mlf.offset - lf.offset;
-				else
-					match = lf.offset - mlf.offset;
+		} // for through merged faces
 
-				AddMergedTriangle(lf, lf_modelspace, mlf, UniqueVerts, UniqueIndices);
+		// special case
+		if (best_matching_merged_face == -1)
+		{
+			String sz = "\tface " + itos(f);
+			sz += " no match";
+			print_line (sz);
+		}
+		else
+		{
+			// standardized order is preprocessed
+			LFace mlf = merged.m_LFaces[best_matching_merged_face];
 
-			}
-//		} // for repeat
+			AddMergedTriangle(lf, lf_modelspace, mlf, UniqueVerts, UniqueIndices);
+		}
 
 	} // for through original faces
 
@@ -355,7 +280,7 @@ void UnMerger::AddMergedTriangle(const LFace &forig, const LFace &forig_modelspa
 		uvert.m_UV = forig.m_UVs[c];
 
 		// 2nd UV from the merged
-		uvert.m_UV2 = fmerged.m_UV2s[c];
+		uvert.m_UV2 = fmerged.m_UVs[c];
 
 		// find it or add to the list
 		int ind_uni_vert = FindOrAddVert(UniqueVerts, uvert);
@@ -363,44 +288,6 @@ void UnMerger::AddMergedTriangle(const LFace &forig, const LFace &forig_modelspa
 		// add the index to form the triangle list
 		UniqueIndices.push_back(ind_uni_vert);
 	}
-
-/*
-	//sz += " match found offset " + itos(match);
-
-	// find the corresponding uv2s in the merged mesh face and add them (taking into account offset)
-	Vector2 found_uvs[3];
-	for (int c=0; c<3; c++)
-	{
-		found_uvs[c] = merged.m_UV2s[mlf.m_index[c]];
-	}
-
-	// add them
-	for (int c=0; c<3; c++)
-	{
-		int which = (c + match)	% 3;
-
-		// index for the uv2 should be the same as the vertex index in the 'to mesh' face
-		int ind = lf.m_index[c];
-		uv2s.set(ind, found_uvs[which]);
-
-		{
-			// construct the unique vert
-			LVert uvert;
-			int orig_ind = lf.m_index[c];
-			uvert.m_Pos = verts[orig_ind];
-			uvert.m_Norm = norms[orig_ind];
-			uvert.m_UV = uv1s[orig_ind];
-			uvert.m_UV2 = found_uvs[which];
-
-			// find it or add to the list
-			int ind_uni_vert = FindOrAddVert(UniqueVerts, uvert);
-
-			// add the index to form the triangle list
-			UniqueIndices.push_back(ind_uni_vert);
-		}
-	}
-*/
-
 }
 
 bool UnMerger::FillMergedFromMesh(LMerged &merged, const MeshInstance &mesh)
@@ -464,8 +351,12 @@ bool UnMerger::FillMergedFromMesh(LMerged &merged, const MeshInstance &mesh)
 			mlf.m_Norm[c] = merged.m_Norms[ind].normalized();
 			mlf.m_index[c] = ind;
 
-			mlf.m_UV2s[c] = merged.m_UV2s[ind];
+			mlf.m_UVs[c] = merged.m_UV2s[ind];
 		}
+
+		// we precalculate the order of the merged faces .. these will be gone through lots of times
+		mlf.StandardizeOrder();
+
 		merged.m_LFaces.push_back(mlf);
 	}
 
@@ -500,63 +391,10 @@ void UnMerger::Transform_Norms(const PoolVector<Vector3> &normsLocal, PoolVector
 
 
 
-// -1 for no match, or 0 for 0 offset match, 1 for +1, 2 for +2 offset match...
-int UnMerger::DoFacesMatch(const LFace& sob_f, const LFace &m_face) const
-{
-	// match one
-	for (int offset = 0; offset < 3; offset++)
-	{
-		if (DoFaceVertsApproxMatch(sob_f, m_face, 0, offset, false))
-		{
-			int res = DoFacesMatch_Offset(sob_f, m_face, offset);
-
-			if (res != -1)
-				return res;
-		}
-	}
-
-	return -1; // no match
-}
-
-int UnMerger::DoFacesMatch_Offset(const LFace& sob_f, const LFace &m_face, int offset) const
-{
-#ifdef LDEBUG_UNMERGE
-	// debug
-	String sz = "\t\tPOSS match sob : ";
-	sz += sob_f.ToString();
-	sz += "\n\t\t\tmerged : ";
-	sz += m_face.ToString();
-	LPRINT(2, sz);
-#endif
-
-
-	// does 2nd and third match?
-	int offset1 = (offset + 1) % 3;
-	if (!DoFaceVertsApproxMatch(sob_f, m_face, 1, offset1, true))
-		return -1;
-
-	int offset2 = (offset + 2) % 3;
-	if (!DoFaceVertsApproxMatch(sob_f, m_face, 2, offset2, true))
-		return -1;
-
-	return offset;
-}
-
-
-bool UnMerger::DoFaceVertsApproxMatch(const LFace& sob_f, const LFace &m_face, int c0, int c1, bool bDebug) const
-{
-	return DoPosNormsApproxMatch(sob_f.m_Pos[c0], sob_f.m_Norm[c0], m_face.m_Pos[c1], m_face.m_Norm[c1], bDebug);
-}
-
 // returns -1 if no match, or the offset 0 1 or 2
-float UnMerger::DoFacesMatch_New(const LFace &a, const LFace &b) const
+float UnMerger::DoFacesMatch(const LFace &a, const LFace &b) const
 {
-	// first standardize both faces
-//	int offset_a = a.StandardizeOrder();
-//	int offset_b = b.StandardizeOrder();
-
-
-//	const float thresh_diff = m_MergeParams.m_fThresholdDist;
+	//	const float thresh_diff = m_MergeParams.m_fThresholdDist;
 	const float thresh_diff_squared = m_MergeParams.m_fThresholdDist_Squared;
 
 	// now we can compare by tuple
@@ -567,100 +405,34 @@ float UnMerger::DoFacesMatch_New(const LFace &a, const LFace &b) const
 		// positions
 		Vector3 pos_diff = b.m_Pos[n] - a.m_Pos[n];
 		float sl = pos_diff.length_squared();
+
+		// quick reject
 		if (sl > thresh_diff_squared)
 			return FLT_MAX;
 
 		goodness_of_fit += sl;
 
 		// normals
-		Vector3 norm_a = a.m_Norm[n];
-		Vector3 norm_b = b.m_Norm[n];
+		// CHECKING THE NORMALS was resulting in some incorrect results. xatlas must change them.
+		// But in fact now the pos checks are accurate, with vertex ordering, back faces will not be selected
+		// so normals don't need checking.
 
-		norm_a.normalize();
-		norm_b.normalize();
+		//		Vector3 norm_a = a.m_Norm[n];
+		//		Vector3 norm_b = b.m_Norm[n];
 
-		float dot = norm_a.dot(norm_b);
+		//		norm_a.normalize();
+		//		norm_b.normalize();
 
-		if (dot < m_MergeParams.m_fThresholdDot)
-			return FLT_MAX;
+		// quick reject
+		//float dot = norm_a.dot(norm_b);
+		//if (dot < m_MergeParams.m_fThresholdDot)
+		//	return FLT_MAX;
 	}
 
-	// if we got to here they matched.
-	// what is their offset?
-//	if (offset_a >= offset_b)
-//		return offset_a - offset_b;
-
-//	return offset_b - offset_a;
+	// if we got to here they matched to some degree
 	return goodness_of_fit;
 }
 
-
-bool UnMerger::DoPosNormsApproxMatch(const Vector3 &a_pos, const Vector3 &a_norm, const Vector3 &b_pos, const Vector3 &b_norm, bool bDebug) const
-{
-	bDebug = false;
-
-	float thresh_diff = m_MergeParams.m_fThresholdDist;
-	float thresh_diff_squared = m_MergeParams.m_fThresholdDist_Squared;
-
-	float x_diff = fabs (b_pos.x - a_pos.x);
-	if (x_diff > thresh_diff)
-	{
-#ifdef LDEBUG_UNMERGE
-		if (bDebug)
-			LPRINT(5, "\t\t\t\tRejecting x_diff " + ftos(x_diff));
-#endif
-		return false;
-	}
-
-	float z_diff = fabs (b_pos.z - a_pos.z);
-	if (z_diff > thresh_diff)
-	{
-#ifdef LDEBUG_UNMERGE
-		if (bDebug)
-			LPRINT(5, "\t\t\t\tRejecting z_diff " + ftos(z_diff));
-#endif
-		return false;
-	}
-
-	float y_diff = fabs (b_pos.y - a_pos.y);
-	if (y_diff > thresh_diff)
-	{
-#ifdef LDEBUG_UNMERGE
-		if (bDebug)
-			LPRINT(5, "\t\t\t\tRejecting y_diff " + ftos(y_diff));
-#endif
-		return false;
-	}
-
-
-
-	Vector3 pos_diff = b_pos - a_pos;
-	if (pos_diff.length_squared() > thresh_diff_squared) // 0.1
-	{
-#ifdef LDEBUG_UNMERGE
-		if (bDebug)
-			LPRINT(5, "\t\t\t\tRejecting length squared " + ftos(pos_diff.length_squared()));
-#endif
-
-		return false;
-	}
-
-	// make sure both are normalized
-	Vector3 na = a_norm;//.normalized();
-	Vector3 nb = b_norm;//.normalized();
-
-	float norm_dot = na.dot(nb);
-	if (norm_dot < m_MergeParams.m_fThresholdDot)
-	{
-#ifdef LDEBUG_UNMERGE
-		if (bDebug)
-			LPRINT(5, "\t\t\t\tRejecting normal " + ftos(norm_dot) + " na : " + String(na) + ", nb : " + String(nb));
-#endif
-		return false;
-	}
-
-	return true;
-}
 
 int UnMerger::FindOrAddVert(LVector<LVert> &uni_verts, const LVert &vert) const
 {
