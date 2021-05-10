7c3ea30bbe3e7a3636b860c8ee6be764e56d8e8e
lawnjelly <lawnjelly@gmail.com>
2021-05-10 15:08:44 +0100

    Docs

--- modules/lportal/config.py
+++ modules/lportal/config.py
@@ -1,5 +1,15 @@
 # config.py
 
+def get_doc_path():
+    return "doc_classes"
+
+def get_doc_classes():
+    return [
+    "LPortal",
+    "LRoom",
+    "LRoomManager",
+    ]
+
 def can_build(env, platform):
     return True
 
--- modules/lsimd/config.py
+++ modules/lsimd/config.py
@@ -1,5 +1,16 @@
 # config.py
 
+def get_doc_path():
+    return "doc_classes"
+
+def get_doc_classes():
+    return [
+    "FastArray_2f32",
+    "FastArray_4f32",
+    "FastArray_4i32",
+    "Vec4_i32",
+    ]
+
 def can_build(env, platform):
     return True
 
