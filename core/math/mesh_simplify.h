7c3c4d9e07908fb7b8c93f582523d48153603344
lawnjelly <lawnjelly@gmail.com>
2025-10-08 14:09:03 +0100

    Progressive LOD

---
+++ core/math/mesh_simplify.h
@@ -0,0 +1,482 @@
+#pragma once
+
+#include "core/local_vector.h"
+#include "core/math/aabb.h"
+#include "core/math/plane.h"
+#include "core/math/spatial_deduplicator.h"
+#include "core/math/vector3.h"
+
+#define GODOT_MESH_SIMPLIFY_USE_DIRTY_VERTS
+#define GODOT_MESH_SIMPLIFY_OPTIMIZED_NEIGHS
+
+class MeshSimplify {
+	SpatialDeduplicator _deduplicator;
+
+	struct Edge {
+		Edge() {
+			a = UINT32_MAX;
+			b = UINT32_MAX;
+		}
+		void sort() {
+			if (b > a) {
+				SWAP(a, b);
+			}
+		}
+		bool operator==(const Edge &p_o) const { return (a == p_o.a) && (b == p_o.b); }
+		uint32_t a, b;
+	};
+
+	struct Tri {
+		Tri() {
+			for (int n = 0; n < 3; n++) {
+				corn[n] = UINT32_MAX;
+				neigh[n] = UINT32_MAX;
+			}
+			aabb_volume = 0.0;
+		}
+		// corner indices
+		uint32_t corn[3];
+
+		Edge edge[3];
+		Plane plane;
+
+		// neighboursing triangles
+		uint32_t neigh[3];
+		uint32_t num_neighs = 0;
+
+		bool active = true;
+
+		// quicker detection of mirror verts by
+		// quick rejection
+		bool has_mirror = false;
+
+		// for detecting mirroring
+		AABB aabb;
+		real_t aabb_volume;
+	};
+
+	struct Vert {
+		Vert() {
+			edge_vert_neighs[0] = UINT32_MAX;
+			edge_vert_neighs[1] = UINT32_MAX;
+			mirror_vert = UINT32_MAX;
+		}
+
+		void link_tri(uint32_t p_id) {
+			int64_t res = tris.find(p_id);
+			if (res == -1) {
+				tris.push_back(p_id);
+			}
+		}
+		void add_edge_vert_neigh(uint32_t p_vert_id) {
+			edge_vert = true;
+			// already present?
+			if (has_edge_vert_neigh(p_vert_id)) {
+				return;
+			}
+
+			// first neighbour?
+			if (edge_vert_neighs[0] == UINT32_MAX) {
+				edge_vert_neighs[0] = p_vert_id;
+				check_edge_vert_neighs();
+				return;
+			}
+
+			// should only be two neighbours possible,
+			// except edge t junctions, and this is a special case
+			if (edge_vert_neighs[1] == UINT32_MAX) {
+				edge_vert_neighs[1] = p_vert_id;
+				check_edge_vert_neighs();
+				return;
+			}
+
+			// special case, more than 2 edge neighbours,
+			// lock the vert
+			locked = true;
+			edge_vert_neighs[0] = UINT32_MAX;
+			edge_vert_neighs[1] = UINT32_MAX;
+		}
+
+		Vector3 pos;
+
+		// If mirroring is detected on an axis,
+		// a vert can have a mirror which can also be merged
+		// at the same time to give symmetrical LODs
+		uint32_t mirror_vert;
+
+		// List of triangles that use this vertex
+		LocalVectori<uint32_t> tris;
+
+		// ancestors
+		//LocalVectori<uint32_t> ancestral_tris;
+		LocalVectori<uint32_t> ancestral_verts;
+
+		// list of vertices that this vertex is already registered to collapse
+		// to on the heap
+		LocalVectori<uint32_t> heap_collapse_to;
+		//LocalVectori<uint32_t> heap_collapse_from;
+
+		// List of verts that share the same position
+		// (these will usually be on another edge, and separated
+		// as a result of UVs or normals).
+		// When collapsing edge verts, we will only do if we can
+		// also similarly collapse the linked vert, to preserve the shared
+		// edge between the two zones (otherwise you get an ugly seam
+		// like Blender decimate).
+		LocalVectori<uint32_t> linked_verts;
+		bool is_linked_to(uint32_t p_vert_id) const {
+			return linked_verts.find(p_vert_id) != -1;
+		}
+
+		// A vertex is active until it has been collapsed
+		bool active = false;
+
+		// if a vert has more than 2 edge neighbours
+		// (i.e. at a t junction of edges)
+		// lock it to prevent collapse, we want to preserve these cases
+		bool locked = false;
+
+		// If we are an edge vert, we will have neighbouring edge verts.
+		// We can use these to determine whether a collapse is allowed along the edge...
+		// A straight line is ok to collapse, but e.g. a right angle will change the outline too much.
+		bool edge_vert = false;
+
+		// Colinear is ok for collapsing to neighbouring edge vert, but non colinear is not
+		bool edge_colinear = false;
+
+		// The neighbouring verts on either side if we follow this edge
+		uint32_t edge_vert_neighs[2];
+
+		// Some helpful edge funcs.
+		bool edge_vert_neighs_same() const { return edge_vert_neighs[0] == edge_vert_neighs[1]; }
+		void check_edge_vert_neighs() {
+			if (edge_vert_neighs[0] != UINT32_MAX) {
+				DEV_ASSERT(!edge_vert_neighs_same());
+			}
+		}
+
+		//		uint32_t count_edge_vert_neighs() const {
+		//			uint32_t count = 0;
+		//			if (edge_vert_neighs[0] != UINT32_MAX)
+		//				count++;
+		//			if (edge_vert_neighs[1] != UINT32_MAX)
+		//				count++;
+		//			return count;
+		//		}
+		uint32_t get_other_edge_vert_neigh(uint32_t p_first) const {
+			if (edge_vert_neighs[0] == p_first) {
+				return edge_vert_neighs[1];
+			}
+			DEV_ASSERT(edge_vert_neighs[1] == p_first);
+			return edge_vert_neighs[0];
+		}
+		void exchange_edge_vert_neigh(uint32_t p_from, uint32_t p_to) {
+			if (edge_vert_neighs[0] == p_from) {
+				edge_vert_neighs[0] = p_to;
+				if (edge_vert_neighs_same()) {
+					edge_vert_neighs[0] = UINT32_MAX;
+				}
+				check_edge_vert_neighs();
+				return;
+			}
+			DEV_ASSERT(edge_vert_neighs[1] == p_from);
+			edge_vert_neighs[1] = p_to;
+			if (edge_vert_neighs_same()) {
+				edge_vert_neighs[1] = UINT32_MAX;
+			}
+			check_edge_vert_neighs();
+		}
+		void set_other_edge_vert_neigh(uint32_t p_keep, uint32_t p_change) {
+			if (p_keep == p_change) {
+				;
+			}
+
+			if (edge_vert_neighs[0] == p_keep) {
+				edge_vert_neighs[1] = p_change;
+				if (edge_vert_neighs_same()) {
+					edge_vert_neighs[1] = UINT32_MAX;
+				}
+				check_edge_vert_neighs();
+				return;
+			}
+			DEV_ASSERT(edge_vert_neighs[1] == p_keep);
+			edge_vert_neighs[0] = p_change;
+			if (edge_vert_neighs_same()) {
+				edge_vert_neighs[0] = UINT32_MAX;
+			}
+			check_edge_vert_neighs();
+		}
+		bool has_edge_vert_neigh(uint32_t p_vert_id) const {
+			return (edge_vert_neighs[0] == p_vert_id) || (edge_vert_neighs[1] == p_vert_id);
+		}
+
+		// We only need to check dirty vertices for collapsing from..
+		// i.e. vertices that have recently had neighbours collapsed.
+		// If we have already checked a vertex for collapsing and no neighbours
+		// have changed, no need to check again in a new iteration.
+#ifdef GODOT_MESH_SIMPLIFY_USE_DIRTY_VERTS
+		bool dirty = true;
+#endif
+
+		// The max displacement of the points merged to this vertex so far.
+		// This prevents "creep", where a vertex merges slowly a large displacement
+		// than would be possible over a single merge
+		real_t displacement = 0.0;
+	};
+
+	struct Collapse {
+		Collapse() {
+			from = UINT32_MAX;
+			to = UINT32_MAX;
+			//error = 0.0;
+		}
+		bool operator==(const Collapse &p_o) const { return (from == p_o.from) && (to == p_o.to); }
+
+		union {
+			struct {
+				uint32_t from;
+				uint32_t to;
+			};
+			uint32_t ids[2];
+		};
+		//real_t error;
+	};
+
+	struct CollapseGroup {
+		CollapseGroup() {
+			error = 0.0;
+			size = 0;
+		}
+		Collapse c[4];
+
+		// number of collapses used
+		uint32_t size;
+
+		// 1 = regular collapse
+		// 2 = mirror collapse or edge collapse
+		// 4 = mirror edge collapse
+
+		// highest error.
+		// apply a negative offset for mirror collapses, so they get processed first
+		float error;
+
+		// disallow if two collapses contain the same verts
+		bool is_invalid() const {
+			for (int n = 0; n < size; n++) {
+				for (int i = 0; i < 2; i++) {
+					uint32_t id = c[n].ids[i];
+
+					for (int m = n + 1; m < size; m++) {
+						if ((c[m].ids[0] == id) || (c[m].ids[1] == id))
+							return true;
+					}
+				}
+			}
+			return false;
+		}
+
+		bool contains_collapse(const Collapse &p_c) const {
+			for (int n = 0; n < 4; n++) {
+				if (c[n] == p_c)
+					return true;
+			}
+			return false;
+		}
+
+		bool contains(uint32_t p_id) const {
+			for (uint32_t n = 0; n < size; n++) {
+				const Collapse &co = c[n];
+				if ((co.from == p_id) || (co.to == p_id))
+					return true;
+			}
+
+			return false;
+		}
+
+		void add_collapse(uint32_t p_from, uint32_t p_to, float p_error) {
+			Collapse &co = c[size];
+			co.from = p_from;
+			co.to = p_to;
+			error = MAX(error, p_error);
+			size++;
+			DEV_ASSERT(size <= 4);
+		}
+
+		// we actually want the best at the end, so they are cheaper to pop
+		bool operator<(const CollapseGroup &p_o) const { return error > p_o.error; }
+	};
+
+public:
+	void add_attribute(const SpatialDeduplicator::Attribute &p_attr) {
+		_deduplicator._attributes.push_back(p_attr);
+	}
+	void clear_attributes() {
+		_deduplicator._attributes.clear();
+	}
+
+	// returns number of indices
+	uint32_t simplify_occluders(const uint32_t *p_in_inds, uint32_t p_num_in_inds, const Vector3 *p_in_verts, uint32_t p_num_in_verts, uint32_t *r_out_inds, Vector3 *r_out_verts, uint32_t &r_num_out_verts, real_t p_simplification, real_t p_vertex_tolerance);
+
+	// target fraction is number of target tris / start tris
+	// surf error is the allowed error over a surface (0 to 1)
+	// edge simplification is the amount of angle in edges allowed to be simplified (0 is no edge simp)
+	uint32_t simplify_map(const uint32_t *p_in_inds, uint32_t p_num_in_inds, const Vector3 *p_in_verts, uint32_t p_num_in_verts, uint32_t *r_out_inds, LocalVectori<uint32_t> &r_vert_map, uint32_t &r_num_out_verts, real_t p_tri_target_fraction = 0.01, real_t p_surf_detail = 1.0, real_t p_edge_simplification = 1.0, real_t p_vertex_tolerance = 0.01);
+
+private:
+	void _finalize_merge(uint32_t p_vert_from_id, uint32_t p_vert_to_id);
+
+	//void _add_collapse(uint32_t p_vert_from_id, uint32_t p_vert_to_id, real_t p_max_displacement);
+
+	void _create_tris(const uint32_t *p_inds, uint32_t p_num_inds);
+	void _establish_neighbours_for_tri(int p_tri_id);
+	void _establish_neighbours(int p_tri0, int p_tri1);
+
+	uint32_t _choose_vert_to_merge(uint32_t p_start_from) const;
+	bool _find_vert_to_merge_to(uint32_t p_vert_from);
+	void _adjust_tri(uint32_t p_tri_id, uint32_t p_vert_from, uint32_t p_vert_to);
+	void _resync_tri(uint32_t p_tri_id);
+	bool _calculate_plane(uint32_t p_corns[3], Plane &r_plane) const;
+	bool _allow_collapse(uint32_t p_tri_id, uint32_t p_vert_test, uint32_t p_vert_from, uint32_t p_vert_to, real_t &r_dist) const;
+	void _delete_triangle(uint32_t p_tri_id);
+
+	bool _is_reciprocal_edge(uint32_t p_vert_a, uint32_t p_vert_b) const;
+
+	void _optimize_vertex_cache(uint32_t *r_inds, uint32_t p_num_inds, uint32_t p_num_verts) const;
+	void _detect_mirror_verts();
+	bool _detect_mirror_tris(const Tri &p_a, const Tri &p_b, int &r_axis, real_t &r_midpoint) const;
+	void _find_mirror_verts(const Tri &p_a, const Tri &p_b, int p_axis, real_t p_midpoint);
+
+	void _debug_verify_vert(uint32_t p_vert_id);
+	void _debug_verify_verts();
+
+	// NEW
+	void _create_heap();
+	bool _evaluate_collapse(uint32_t p_vert_from_id, uint32_t p_vert_to_id, CollapseGroup &r_cg, bool p_second_edge = false);
+	void _evaluate_collapse_group(uint32_t p_vert_from_id, uint32_t p_vert_to_id);
+	bool _evaluate_collapses_from_vertex(uint32_t p_vert_from_id);
+	void _reevaluate_from_changed_vertex(uint32_t p_deleted_vert, uint32_t p_central_vert);
+	void _heap_remove(uint32_t p_vert_id);
+	void _destroy_collapse_group(const CollapseGroup &p_cg);
+	//uint32_t _heap_find_collapse(uint32_t p_vert_from_id, uint32_t p_vert_to_id);
+
+	bool _new_simplify();
+	bool _collapse_matching_edge(const Collapse &p_collapse);
+
+	bool _cg_creates_flipped_tris(const CollapseGroup &p_cg);
+
+	bool _find_second_edge_from_to(uint32_t p_vert_from_id, uint32_t p_vert_to_id, uint32_t &r_vert_from_id, uint32_t &r_vert_to_id) const;
+	bool _are_verts_linked(uint32_t p_id_a, uint32_t p_id_b) const;
+
+	// after collapsing to a vertex, and adding an ancestral vert,
+	// the cost of collapsing this vert to others has changed and needs recalculating
+	void _recalculate_collapse_errors_from_vert(uint32_t p_vert_merge_id);
+	bool _evaluate_collapse_metric(uint32_t p_vert_from_id, uint32_t p_vert_to_id, real_t &r_error) const;
+
+	void _debug_print_collapse_group(const CollapseGroup &p_cg, int p_tabs = 0, String p_title = String()) const;
+
+	LocalVectori<Vert> _verts;
+	LocalVectori<Tri> _tris;
+	uint32_t _active_tri_count = 0;
+
+	LocalVectori<uint32_t> _merge_vert_ids;
+	bool _mirror_verts_only = false;
+	real_t _edge_simplification = 1.0;
+	real_t _allowed_surface_error = 1.0;
+
+	// pending list of collapses, so we can backtrack and undo
+	// collapses in the case of mirror verts
+	//LocalVectori<Collapse> _collapses;
+
+	// sorted heap of collapses
+	LocalVectori<CollapseGroup> _heap;
+
+	// we don't add new collapses directly to the heap..
+	// this saves us doing lots of sorting, and also makes the verts
+	// mirroring more likely to work correctly.
+	LocalVectori<CollapseGroup> _heap_pending;
+
+	void _consolidate_heap();
+
+	// This temporary triangle list is used multiple times,
+	// and is stored on the object instead of recreating each time
+	// to save on allocations.
+	LocalVectori<uint32_t> _possible_tris;
+
+	bool dot_edge_test(const Vector3 &p_a, const Vector3 &p_b, const Vector3 &p_c) const {
+		if (_edge_simplification > 0.0) {
+			// never allow edge
+			if (_edge_simplification == 1.0)
+				return false;
+
+			Vector3 a = p_b - p_a;
+			Vector3 b = p_c - p_b;
+
+			real_t la = a.length();
+			real_t lb = b.length();
+
+			// always pass if points are close together, we want to get rid of this edge vert
+			if ((la < 0.001) || (lb < 0.001))
+				return true;
+
+			// normalize
+			a *= 1.0 / la;
+			b *= 1.0 / lb;
+
+			real_t dot = a.dot(b);
+			if (dot < _edge_simplification)
+				return false;
+		}
+
+		// always allow edge
+		return true;
+	}
+
+	// Adapted from https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line
+	bool dist_point_from_line(const Vector3 &p_pt, const Vector3 &p_a, const Vector3 &p_b, real_t &r_dist) const {
+		Vector3 n = p_b - p_a;
+		real_t n_l = n.length();
+
+		// line is undefined in direction, because the two points are too close / zero..
+		// just return distance to the points.
+		if (n_l < 0.0001) {
+			r_dist = (p_pt - p_a).length();
+			return false;
+		}
+
+		Vector3 p_min_a = p_pt - p_a;
+		Vector3 cross = p_min_a.cross(n);
+		real_t l = cross.length();
+
+		r_dist = l / n_l;
+		return true;
+	}
+
+	/*
+	bool _try_simplify_merge_vert(Vert &p_vert_from, uint32_t p_vert_from_id, uint32_t p_vert_to_id, real_t &r_max_displacement) const {
+		const Vert &vert_to = _verts[p_vert_to_id];
+
+		// disallow if edge verts and not linked
+		if (p_vert_from.edge_vert || vert_to.edge_vert) {
+			if (!_is_reciprocal_edge(p_vert_from_id, p_vert_to_id))
+				return false;
+		}
+
+		r_max_displacement = 0.0;
+
+		// mirror vert only?
+		if (_mirror_verts_only) {
+			if (vert_to.mirror_vert == UINT32_MAX)
+				return false;
+		}
+
+		// is this suitable for collapse?
+		for (int n = 0; n < p_vert_from.tris.size(); n++) {
+			if (!_allow_collapse(p_vert_from.tris[n], p_vert_from_id, p_vert_to_id, r_max_displacement)) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+	*/
+};
---
+++ core/math/spatial_deduplicator.cpp
@@ -0,0 +1,242 @@
+#include "spatial_deduplicator.h"
+#include "core/print_string.h"
+#include "core/variant.h"
+
+AABB SpatialDeduplicator::Grid::calc_bound(const Vector3 *p_verts, uint32_t p_num_verts) {
+	if (!p_num_verts) {
+		return AABB();
+	}
+
+	AABB aabb;
+	aabb.position = p_verts[0];
+	for (uint32_t n = 1; n < p_num_verts; n++) {
+		aabb.expand_to(p_verts[n]);
+	}
+
+	Rect2 rt;
+	rt.position = vec3_xy(aabb.position);
+	rt.size = vec3_xy(aabb.size);
+
+	//	rt.position = vec3_xy(p_verts[0]);
+
+	//	for (uint32_t n = 0; n < p_num_verts; n++) {
+	//		rt.expand_to(vec3_xy(p_verts[n]));
+	//	}
+
+	_bound_min = rt.position;
+	_bound_mult = Vector2();
+	if (rt.size.x > 0.0) {
+		_bound_mult.x = GRID_SIZE / rt.size.x;
+	}
+	if (rt.size.y > 0.0) {
+		_bound_mult.y = GRID_SIZE / rt.size.y;
+	}
+
+	return aabb;
+}
+
+void SpatialDeduplicator::Grid::add(const Vector3 &p_pos, uint32_t p_id) {
+	Vector2i loc = get_cell_xy(p_pos);
+	Vert v;
+	v.id = p_id;
+	v.pos = p_pos;
+
+	for (int y = loc.y - 1; y <= loc.y + 1; y++) {
+		for (int x = loc.x - 1; x <= loc.x + 1; x++) {
+			if (is_on_map(x, y)) {
+				Cell &cell = get_cell(x, y);
+				cell.verts.push_back(v);
+			}
+		}
+	}
+}
+
+bool SpatialDeduplicator::Grid::find(const Vector3 &p_pos, real_t p_epsilon, LocalVectori<uint32_t> &r_ids) const {
+	r_ids.clear();
+
+	const Cell &cell = get_cell_at_pos(p_pos);
+	for (int n = 0; n < cell.verts.size(); n++) {
+		const Vert &v = cell.verts[n];
+		if (p_pos.is_equal_approx(v.pos, p_epsilon)) {
+			//return v.id;
+			r_ids.push_back(v.id);
+		}
+	}
+
+	return r_ids.size() != 0;
+}
+
+bool SpatialDeduplicator::deduplicate_verts_only(const uint32_t *p_in_inds, uint32_t p_num_in_inds, const Vector3 *p_in_verts, uint32_t p_num_in_verts, LocalVectori<Vector3> &r_out_verts, LocalVectori<uint32_t> &r_out_inds, real_t p_epsilon) {
+	LocalVectori<uint32_t> vert_map;
+	uint32_t num_out_verts = 0;
+	if (!deduplicate_map(p_in_inds, p_num_in_inds, p_in_verts, p_num_in_verts, vert_map, num_out_verts, r_out_inds, nullptr, p_epsilon))
+		return false;
+
+	// create new verts list
+	r_out_verts.resize(num_out_verts);
+
+	for (int n = 0; n < p_num_in_verts; n++) {
+		uint32_t new_vert_id = vert_map[n];
+		DEV_ASSERT(new_vert_id < num_out_verts);
+
+		r_out_verts[new_vert_id] = p_in_verts[n];
+	}
+
+	return true;
+}
+
+// The spatial deduplication is done automatically, but the user can provide a template function following the form above
+// to detect whether a vertex is similar enough to be merged based on e.g. normal, UVs etc.
+bool SpatialDeduplicator::deduplicate_map(const uint32_t *p_in_inds, uint32_t p_num_in_inds, const Vector3 *p_in_verts, uint32_t p_num_in_verts, LocalVectori<uint32_t> &r_vert_map, uint32_t &r_num_out_verts, LocalVectori<uint32_t> &r_out_inds, AABB *r_bound, real_t p_epsilon) {
+	_epsilon = p_epsilon;
+
+	//LocalVectori<uint32_t> vert_map;
+	// vert map needs to be big enough for all the input verts,
+	// it maps an input vert to an output vert after removing duplicates
+	r_vert_map.resize(p_num_in_verts);
+
+	LocalVectori<Vector3> out_verts;
+	LocalVectori<uint32_t> out_vert_sources;
+	out_verts.clear();
+	r_out_inds.clear();
+
+#define GODOT_DEDUPLICATOR_USE_GRID
+#ifdef GODOT_DEDUPLICATOR_USE_GRID
+	Grid grid;
+	AABB world_bound = grid.calc_bound(p_in_verts, p_num_in_verts);
+	if (r_bound) {
+		*r_bound = world_bound;
+	}
+	LocalVectori<uint32_t> found_ids;
+#endif
+
+	for (int n = 0; n < p_num_in_verts; n++) {
+		const Vector3 &pt = p_in_verts[n];
+
+		//print_line("in_vert " + itos(n) + " : ( " + String(Variant(pt)) + " ) ");
+
+		// find existing?
+		bool found = false;
+
+#ifdef GODOT_DEDUPLICATOR_USE_GRID
+		grid.find(pt, _epsilon, found_ids);
+
+		for (int idc = 0; idc < found_ids.size(); idc++) {
+			int i = found_ids[idc];
+			{
+#else
+		for (int i = 0; i < out_verts.size(); i++) {
+			//print_line("\tout_vert " + itos(i) + " : ( " + String(Variant(out_verts[i])) + " ) ");
+
+			if (out_verts[i].is_equal_approx(pt, _epsilon)) {
+#endif
+				// attribute tests
+				int out_vert_orig_id = out_vert_sources[i];
+
+				bool allow = true;
+				for (int t = 0; t < _attributes.size(); t++) {
+					const Attribute &attr = _attributes[t];
+					switch (attr.type) {
+						case Attribute::AT_POSITION: {
+							//DEV_ASSERT(attr.vec3s[out_vert_orig_id] == pt);
+							const Vector3 &a = attr.vec3s[n];
+							const Vector3 &b = attr.vec3s[out_vert_orig_id];
+							real_t sl = (b - a).length_squared();
+							if (sl > attr.epsilon_dedup) {
+								allow = false;
+								break;
+							}
+						} break;
+						case Attribute::AT_NORMAL:
+						default: {
+							const Vector3 &a = attr.vec3s[n];
+							const Vector3 &b = attr.vec3s[out_vert_orig_id];
+							real_t dot = a.dot(b);
+							if (dot < attr.epsilon_dedup) {
+								allow = false;
+								//print_line("disallowing merge at " + String(Variant(pt)));
+								break;
+							}
+						} break;
+						case Attribute::AT_UV: {
+							const Vector2 &a = attr.vec2s[n];
+							const Vector2 &b = attr.vec2s[out_vert_orig_id];
+							real_t sl = (b - a).length_squared();
+							if (sl > attr.epsilon_dedup) {
+								allow = false;
+								break;
+							}
+						} break;
+					}
+				}
+
+				if (allow) {
+					r_vert_map[n] = i;
+					found = true;
+					break;
+				}
+			}
+		}
+
+		if (found)
+			continue;
+
+		// not found...
+		r_vert_map[n] = out_verts.size();
+
+#ifdef GODOT_DEDUPLICATOR_USE_GRID
+		grid.add(pt, out_verts.size());
+#endif
+
+		out_verts.push_back(pt);
+		out_vert_sources.push_back(n);
+	}
+
+	// pass back the number of final verts
+	r_num_out_verts = out_verts.size();
+
+	// sort output data
+	for (int n = 0; n < p_num_in_inds; n++) {
+		uint32_t ind = p_in_inds[n];
+		DEV_ASSERT(ind < p_num_in_verts);
+
+		uint32_t new_ind = r_vert_map[ind];
+		DEV_ASSERT(new_ind < out_verts.size());
+
+		r_out_inds.push_back(new_ind);
+	}
+
+	return true;
+}
+
+void SpatialDeduplicator::find_duplicate_positions(const Vector3 *p_in_verts, uint32_t p_num_in_verts, LocalVectori<LinkedVerts> &r_linked_verts_list, real_t p_epsilon) {
+	// resize down at the end
+	r_linked_verts_list.resize(p_num_in_verts);
+	int used = 0;
+
+	for (uint32_t n = 0; n < p_num_in_verts; n++) {
+		const Vector3 &pos = p_in_verts[n];
+
+		// already exists?
+		bool found = false;
+
+		for (int i = 0; i < used; i++) {
+			LinkedVerts &lv = r_linked_verts_list[i];
+			if (lv.pos.is_equal_approx(pos, p_epsilon)) {
+				lv.vert_ids.push_back(n);
+				found = true;
+				break;
+			}
+		}
+
+		if (!found) {
+			LinkedVerts &lv = r_linked_verts_list[used];
+			lv.pos = pos;
+			lv.vert_ids.push_back(n);
+			used++;
+		}
+	}
+
+	// resize the array down
+	r_linked_verts_list.resize(used);
+}
